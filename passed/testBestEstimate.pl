2860a46e3654c51c9acacf1b6ca67c0579ca0d55
danielhabib3 <danielhabib301@gmail.com>
2025-02-06 15:44:58 +0100

    tests

--- passed/testBestEstimate.pl
+++ passed/testBestEstimate.pl
@@ -72,6 +72,23 @@ move(B, Col, V, B2) :-
     set_item(B, Col, V, B2),
     !.
 
+find_lowest_empty_square(B, Col, E, S) :-
+    find_lowest_empty_square(B, Col, 1, E, S).
+
+find_lowest_empty_square(Board, Col, Row, E, S) :-
+    Row =< 6,  % Ensure we do not go beyond the last row
+    nth1(Row, Board, RowList),  % Get the list representing the current row. rowList = Board[Row]
+    nth1(Col, RowList, E),  % Check if the square in the given column is empty
+    S = Row, !.
+
+find_lowest_empty_square(B, Col, Row, E, S) :-
+    Row < 6,  % Move to the next row if the current one is not empty
+    NextRow is Row + 1,
+    find_lowest_empty_square(B, Col, NextRow, E, S), !.
+
+find_lowest_empty_square(B, Col, Row, E, S) :-
+    S = 7.
+
 
 set_item(B, Col, V, B2) :-
     find_lowest_empty_square(B, Col, 'e', Row),
